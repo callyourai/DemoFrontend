9e9bc59d7d202a8df7be01f5610c908e4cd25039
callyourai <jonaswillaredt@gmail.com>
2024-05-27 23:46:47 -0700

    working demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import { AIVideoCall } from 'aivideocall';
+import 'aivideocall/dist/style.css'; // Importing the CSS from the component
+
 
 function App() {
   return (
-      <div style={{ height: '100%', display: 'relative', width: '100%' }}>
+      <div style={{ height: '100%', display: '', width: '100%' }}>
 
       <AIVideoCall 
             callSession={{ userId:"1bb02726-d0dc-47d3-b235-8e666e87c87b", agentId:"1bb02726-d0dc-47d3-b235-8e666e87c87b" }} 
