5936ea23b55ee8aaa4658c40d5b15511abb98c1e
callyourai <jonaswillaredt@gmail.com>
2024-06-01 17:40:36 -0700

    1.0.4 - send event method exposed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import { AIVideoCall } from 'aivideocall';
+import { AIVideoCall, Event } from 'aivideocall';
 import 'aivideocall/dist/style.css';
-import React  from 'react';
+import React, { useRef } from 'react';
 
 function App() {
-  return (
-      <div style={{ height: '100%', display: '', width: '100%' }}>
+  const videoCallRef = useRef(null);
+  const event = new Event('speak', { userId: "1bb02726-d0dc-47d3-b235-8e666e87c87b", agentId: "1bb02726-d0dc-47d3-b235-8e666e87c87b" }, "1bb02726-d0dc-47d3-b235-8e666e87c871", "1", "en-US", "this is a test")
+  if (videoCallRef.current) {
+    videoCallRef.current.sendEvent(event);
+  } else {
+    console.warn('videoCallRef is not available');
+  }
 
+  return (
+    <div style={{ height: '100%', display: '', width: '100%' }}>
       <AIVideoCall 
-            callSession={{ userId:"1bb02726-d0dc-47d3-b235-8e666e87c87b", agentId:"1bb02726-d0dc-47d3-b235-8e666e87c87b" }} 
-        />
+        ref={videoCallRef}
+        callSession={{ userId: "1bb02726-d0dc-47d3-b235-8e666e87c87b", agentId: "1bb02726-d0dc-47d3-b235-8e666e87c87b" }} 
+      />
     </div>
   );
 }
 
 export default App;
-            
